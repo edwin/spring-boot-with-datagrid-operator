da3b1d521f2446b4f843e7e2a7a34c6924c00ce7
edwin <edwinkun@gmail.com>
2024-09-12 21:19:27 +0700

    get or create cache :pig: :snail: :ram:

--- src/main/java/com/edw/helper/CacheHelper.java
+++ src/main/java/com/edw/helper/CacheHelper.java
@@ -2,6 +2,7 @@ package com.edw.helper;
 
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.commons.configuration.XMLStringConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,37 +33,29 @@ public class CacheHelper {
 
     public void populate() {
         // build cache without persistence
-        RemoteCache cache = cacheManager.getCache("cache-without-persistence");
-        if (cache == null) {
-            cacheManager.administration().createCache("cache-without-persistence",
-            "<distributed-cache name=\"cache-without-persistence\" mode=\"ASYNC\">\n" +
+        RemoteCache cacheWithoutPersistence = cacheManager.administration().getOrCreateCache("cache-without-persistence",
+                new XMLStringConfiguration("<distributed-cache name=\"cache-without-persistence\" mode=\"ASYNC\">\n" +
                         "\t<encoding media-type=\"application/x-jboss-marshalling\" />\n" +
-                    "</distributed-cache>");
-            cache = cacheManager.getCache("cache-without-persistence");
-        }
-
+                        "</distributed-cache>")
+        );
         for (int i = 0; i < 50; i++) {
-            cache.put(i, UUID.randomUUID().toString());
+            cacheWithoutPersistence.put(i, UUID.randomUUID().toString());
         }
 
         // build cache with persistence
-        cache = cacheManager.getCache("cache-with-persistence");
-        if (cache == null) {
-            cacheManager.administration().createCache("cache-with-persistence",
-                    "<distributed-cache name=\"cache-with-persistence\" mode=\"ASYNC\">\n" +
-                            "\t<encoding media-type=\"application/x-jboss-marshalling\" />\n" +
-                            "\t<persistence passivation=\"false\">\n" +
-                            "\t\t<file-store>\n" +
-                            "\t\t  <index path=\"/opt/infinispan/server/data\" />\n" +
-                            "\t\t  <data path=\"/opt/infinispan/server/data\" />\n" +
-                            "\t\t</file-store>\n" +
-                            "\t</persistence>\n" +
-                            "</distributed-cache>");
-            cache = cacheManager.getCache("cache-with-persistence");
-        }
-
+        RemoteCache cacheWithPersistence = cacheManager.administration().getOrCreateCache("cache-with-persistence",
+                new XMLStringConfiguration("<distributed-cache name=\"cache-with-persistence\" mode=\"ASYNC\">\n" +
+                        "\t<encoding media-type=\"application/x-jboss-marshalling\" />\n" +
+                        "\t<persistence passivation=\"false\">\n" +
+                        "\t\t<file-store>\n" +
+                        "\t\t  <index path=\"/opt/infinispan/server/data\" />\n" +
+                        "\t\t  <data path=\"/opt/infinispan/server/data\" />\n" +
+                        "\t\t</file-store>\n" +
+                        "\t</persistence>\n" +
+                        "</distributed-cache>")
+        );
         for (int i = 0; i < 50; i++) {
-            cache.put(i, UUID.randomUUID().toString());
+            cacheWithPersistence.put(i, UUID.randomUUID().toString());
         }
     }
 
